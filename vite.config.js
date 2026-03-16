13fa8ce893c32eadd32768a2ff8506eb8d5ff7e9
Rubén E. <81328415+FoulRune02@users.noreply.github.com>
2026-03-16 14:24:09 +0000

    cambiar base y nuevo deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/FoulRune02.github.io/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
